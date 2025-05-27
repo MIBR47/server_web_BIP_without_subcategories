eda19d64c222629d498a7f58bd8a1746e68cca64
MIBR47 <ilhambachtiar47@gmail.com>
2025-05-27 11:22:34 +0700

    test main.ts ubah untuk deploy ke vps

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,17 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: 'http://localhost:3000', // asal frontend kamu
-    credentials: true, // kalau kamu kirim cookie atau header auth
+    origin: ['http://localhost:3000', 'https://yourfrontend.com'],
+    credentials: true,
   });
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
 
-  await app.listen(process.env.PORT ?? 5000);
+  const port = process.env.PORT ?? 5000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 Server is running on http://localhost:${port}`);
 }
 bootstrap();
