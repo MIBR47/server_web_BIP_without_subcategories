1a2a4fbaf767e3006cf3b845c036207aa42d3933
MIBR47 <ilhambachtiar47@gmail.com>
2025-08-05 15:08:35 +0700

    ubah main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://yourfrontend.com'],
+    origin: ['http://localhost:3000', 'http://mibop.my.id'],
     credentials: true,
   });
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
 
-  const port = process.env.PORT ?? 5000;
+  const port = process.env.PORT ?? 3000;
   await app.listen(port, '0.0.0.0');
   console.log(`🚀 Server is running on http://localhost:${port}`);
 }
